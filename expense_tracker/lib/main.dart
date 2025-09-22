9175a53b3bd85ac191b0ac0db3cc90c95161a051
Fahmi Zidan <zidanf656@gmail.com>
2025-09-23 02:34:12 +0700

    Menambahkan halaman add_expense_page

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:coba/page/login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
