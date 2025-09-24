fc53850db48857f45338c37377f41b86292cf083
Fahmi Zidan <zidanf656@gmail.com>
2025-09-24 20:43:58 +0700

    WIP: Mengerjakan ulang layout dashboard

--- expense_tracker/lib/page/login.dart
+++ expense_tracker/lib/page/login.dart
@@ -1,4 +1,4 @@
-import 'package:coba/page/balance_page.dart';
+import 'package:coba/page/dashboard.dart';
 import 'package:coba/page/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
